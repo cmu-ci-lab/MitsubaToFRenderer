3cd3b063eec854051770a9eaf7ec5500ee4a0e70
Adithya Pediredla <aditya.eee.nitw@gmail.com>
2018-01-21 12:36:56 -0600

    Bug fix: Floating point error in miWeight calculation

--- src/integrators/bdpt/bdpt_proc.cpp
+++ src/integrators/bdpt/bdpt_proc.cpp
@@ -450,7 +450,7 @@ public:
 
 								vs->measure = vt->measure = EArea;
 
-								Float miWeight = 1/(s+t-1);
+								Float miWeight = 1.0/(s+t-1);
 //								Path::miWeight(scene, emitterSubpath, &connectionEdge,
 //									sensorSubpath, s, t, m_config.sampleDirect, m_config.lightImage);
 
@@ -519,7 +519,7 @@ public:
 					/* Compute the multiple importance sampling weight */
 //					Float miWeight = Path::miWeight(scene, emitterSubpath, &connectionEdge,
 //						sensorSubpath, s, t, m_config.sampleDirect, m_config.lightImage);
-					Float miWeight = 1/(s+t-1);
+					Float miWeight = 1.0/(s+t-1);
 
 					if (sampleDirect) {
 						/* Now undo the previous change */
